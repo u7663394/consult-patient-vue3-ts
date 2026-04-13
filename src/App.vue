ca00d11a1cc0867ed1ff8d98f8f0237ba5e839ca
u7663394 <u7663394@anu.edu.au>
2026-04-14 01:56:48 +1000

    Implement theme customization

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,17 @@ import { Button as VantButton } from 'vant'
 </script>
 
 <template>
-  <div>我是 App</div>
   <vant-button type="primary">主要按钮</vant-button>
+  <div class="main">我是 main 区域</div>
+  <div class="footer">我是 footer 区域</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main {
+  background-color: var(--cp-primary);
+}
+
+.footer {
+  background-color: var(--footer-color);
+}
+</style>
