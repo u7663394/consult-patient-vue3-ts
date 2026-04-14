b8abc177c3fffd06f62c5ee88d3b299893f25342
u7663394 <u7663394@anu.edu.au>
2026-04-14 13:42:44 +1000

    Config components auto import

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,6 +2,7 @@
 
 <template>
   <div class="login-page">login</div>
+  <van-button>test</van-button>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// 组件自动注册
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      // 不自动生成组件类型文件
+      dts: false,
+      // main.ts 中已经引入了样式, 此处不再引入样式
+      resolvers: [VantResolver({ importStyle: false })],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
