edac43b47bba0110c38c63395f7becfd627d31ba
u7663394 <u7663394@anu.edu.au>
2026-04-14 15:13:59 +1000

    Implement login page UI and styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="app">
-    <router-view></router-view>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style lang="scss" scoped></style>
