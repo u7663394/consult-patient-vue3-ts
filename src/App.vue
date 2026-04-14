6e36a6455e50fbf1f06a7ff7b72cc7e5f70691b0
u7663394 <u7663394@anu.edu.au>
2026-04-14 13:01:18 +1000

    Extract request functions and create request.ts module

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
 import { Button as VantButton } from 'vant'
 import { useUserStore } from './stores'
+import { request } from './utils/request'
 
 const userStore = useUserStore()
-const onLogin = () => {
-  userStore.setUser({
-    id: '1',
-    account: '张三',
-    mobile: '12345678901',
-    avatar: 'demo.png',
-    token: 'abc123}',
+const onLogin = async () => {
+  const res = await request('login/password', 'post', {
+    mobile: '13811111111',
+    password: '123441123',
   })
+  userStore.setUser(res.data)
 }
+
 const onLogout = () => {
   userStore.delUser()
+}
+
+const getUser = async () => {
+  await request('patient/myUser', 'get')
 }
 </script>
 
@@ -21,6 +25,7 @@ const onLogout = () => {
   <div>用户信息: {{ userStore.user }}</div>
   <vant-button type="primary" @click="onLogin">模拟登陆</vant-button>
   <vant-button type="danger" @click="onLogout">模拟退出</vant-button>
+  <vant-button type="primary" @click="getUser">获取用户信息</vant-button>
 </template>
 
 <style scoped></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,69 @@
+import axios, { type Method } from 'axios'
+import { useUserStore } from '@/stores'
+import router from '@/router'
+import { showToast } from 'vant'
+import type { AxiosRequestConfig } from 'axios'
+
+// 1. 基地址 + 超时时间
+const baseURL = 'https://consult-api.itheima.net/'
+const instance = axios.create({
+  baseURL: baseURL,
+  timeout: 10000,
+})
+
+instance.interceptors.request.use(
+  (config) => {
+    // 2. 携带 token
+    const userStore = useUserStore()
+    if (userStore.user?.token && config.headers) {
+      config.headers['Authorization'] = `Bearer ${userStore.user?.token}`
+    }
+    return config
+  },
+  (err) => Promise.reject(err),
+)
+
+instance.interceptors.response.use(
+  (res) => {
+    // 3. 处理业务失败
+    if (res.data?.code !== 10000) {
+      showToast(res.data?.message || '业务失败')
+      return Promise.reject(res.data)
+    }
+    // 4. 数据剥离
+    return res.data
+  },
+  (err) => {
+    // 5. 处理401错误
+    if (err.response?.status === 401) {
+      // 删除用户信息
+      const userStore = useUserStore()
+      userStore.delUser()
+      // 跳转登录页
+      router.push({
+        path: '/login',
+        // 带上所在页面的地址，登录完成后回跳使用
+        query: { returnUrl: router.currentRoute.value.fullPath },
+      })
+    }
+    return Promise.reject(err)
+  },
+)
+
+export default instance
+export { baseURL }
+
+// 6. 导出请求函数，方便使用
+export const request = (url: string, method: Method = 'GET', submitData?: object) => {
+  const config: AxiosRequestConfig = {
+    url,
+    method,
+  }
+  // 根据 method 判断是传 params 还是 data
+  if (method.toUpperCase() === 'GET') {
+    config.params = submitData
+  } else {
+    config.data = submitData
+  }
+  return instance.request(config)
+}
