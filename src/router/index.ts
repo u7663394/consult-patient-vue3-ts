5aadeca18532d49fb709926c2aaea699065a86b2
u7663394 <u7663394@anu.edu.au>
2026-04-15 13:19:41 +1000

    Configure nested routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,20 @@ import { createRouter, createWebHistory } from 'vue-router'
  */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/login', component: () => import('@/views/Login/index.vue') }],
+  routes: [
+    { path: '/login', component: () => import('@/views/Login/index.vue') },
+    {
+      path: '/',
+      redirect: '/home',
+      component: () => import('@/views/Layout/index.vue'),
+      children: [
+        { path: '/home', component: () => import('@/views/Home/index.vue') },
+        { path: '/article', component: () => import('@/views/Article/index.vue') },
+        { path: '/user', component: () => import('@/views/User/index.vue') },
+        { path: '/notify', component: () => import('@/views/Notify/index.vue') },
+      ],
+    },
+  ],
 })
 
 export default router
