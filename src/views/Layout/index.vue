2a83a922e2271a64ab2c6ae1df42b2468fd05a5b
u7663394 <u7663394@anu.edu.au>
2026-04-15 13:27:03 +1000

    Implement layout tabbar

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -4,8 +4,56 @@
   <div class="layout-page">
     <!-- 二级路由 -->
     <router-view></router-view>
-    <div>tabbar</div>
+    <!-- 
+     底部导航: 
+       1. route: 开启路由模式
+       2. to: 路由地址
+       3. #icon="{ active }" 自定义图标插槽
+       4. active: 组件内部封装的选中状态, true/false
+    -->
+    <van-tabbar route>
+      <van-tabbar-item to="/home">
+        首页
+        <template #icon="{ active }">
+          <cp-icon :name="`home-index-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/article">
+        健康百科
+        <template #icon="{ active }">
+          <cp-icon :name="`home-article-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/notify">
+        消息中心
+        <template #icon="{ active }">
+          <cp-icon :name="`home-notice-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/user">
+        我的
+        <template #icon="{ active }">
+          <cp-icon :name="`home-mine-${active ? 'active' : 'default'}`" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-page {
+  :deep() {
+    .van-tabbar-item {
+      &__icon {
+        font-size: 21px;
+      }
+      &__text {
+        font-size: 11px;
+      }
+      &:not(.van-tabbar-item--active) {
+        color: var(--cp-text3);
+      }
+    }
+  }
+}
+</style>
