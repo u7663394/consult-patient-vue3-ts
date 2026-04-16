3786f9640b3da20bde27aa7800b9ab1c3959f9dd
u7663394 <u7663394@anu.edu.au>
2026-04-16 12:13:23 +1000

    Implement DoctorCard and FollowDoctor components

---
+++ src/views/Home/components/FollowDoctor.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import DoctorCard from './DoctorCard.vue'
+</script>
+
+<template>
+  <div class="follow-doctor">
+    <div className="head">
+      <p>推荐关注</p>
+      <a href="javascript:;"> 查看更多<i class="van-icon van-icon-arrow" /></a>
+    </div>
+    <div class="body">
+      <!-- 
+       swipe 组件:
+         1. width: 每个 swipe-item 的宽度
+         2. show-indicators: 是否显示指示点
+         3. loop: 是否循环滚动
+      -->
+      <van-swipe :width="150" :show-indicators="false" :loop="false">
+        <van-swipe-item v-for="item in 5" :key="item">
+          <DoctorCard></DoctorCard>
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.follow-doctor {
+  background-color: var(--cp-bg);
+  height: 250px;
+  .head {
+    display: flex;
+    justify-content: space-between;
+    height: 45px;
+    align-items: center;
+    padding: 0 15px;
+    font-size: 13px;
+    > a {
+      color: var(--cp-tip);
+    }
+  }
+  .body {
+    width: 100%;
+    overflow: hidden;
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import KnowledgeList from './components/KnowledgeList.vue'
+import FollowDoctor from './components/FollowDoctor.vue'
 const active = ref(1) // Tab 组件 active 索引
 </script>
 
@@ -84,7 +85,10 @@ const active = ref(1) // Tab 组件 active 索引
        2. sticky: 吸顶, 滚动时标签会固定在顶部
     -->
     <van-tabs shrink sticky v-model:active="active">
-      <van-tab title="关注"><knowledge-list type="like" /></van-tab>
+      <van-tab title="关注">
+        <follow-doctor></follow-doctor>
+        <knowledge-list type="like"
+      /></van-tab>
       <van-tab title="推荐"><knowledge-list type="recommend" /></van-tab>
       <van-tab title="减脂"><knowledge-list type="fatReduction" /></van-tab>
       <van-tab title="饮食"><knowledge-list type="food" /></van-tab>
