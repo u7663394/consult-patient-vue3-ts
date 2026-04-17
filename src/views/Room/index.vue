33194f4378df9b122647ee82c86cba7771f1c60f
u7663394 <u7663394@anu.edu.au>
2026-04-17 23:55:53 +1000

    feat: implement Room page component structure and styling

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import RoomAction from './components/RoomAction.vue'
+import RoomMessage from './components/RoomMessage.vue'
+import RoomStatus from './components/RoomStatus.vue'
+</script>
 
 <template>
-  <div>room page</div>
+  <div class="room-page">
+    <cp-nav-bar title="医生问诊室" />
+    <room-status />
+    <room-message></room-message>
+    <room-action></room-action>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>
